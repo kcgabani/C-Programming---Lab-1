4f24862827138e29010b1cea029a61024131535b
kcgabani <kcgabani06@gmail.com>
2025-08-29 10:11:55 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,16 @@
+ #include<stdio.h>
+int main()
+{
+  int n, i=0;
+  printf("enter the value of n:");
+  scanf("%d" , &n);
+  do
+  {
+      i++;
+      printf("%d\n" , i);
+  }
+  while (i<n);
+  return 0;
+}
+
+
