c6cd3e2b42fbfe75d512567fd2427a301a641be0
kcgabani <kcgabani06@gmail.com>
2025-08-29 10:12:55 +0530

    Add files via upload

---
+++ four.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int i, n;
+  printf("enter the value of n:");
+  scanf("%d" , &n);
+  printf("natural numbers from 1 to %d are:\n, n");
+ for(i =1;i<=n;i++)
+  {
+      printf("%d\n" , i);
+  }
+  return 0;
+}
+
+
+
