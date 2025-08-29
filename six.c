597eea5acace4860eec45c66dc355825e0c3cacd
kcgabani <kcgabani06@gmail.com>
2025-08-29 10:13:25 +0530

    Add files via upload

---
+++ six.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int n, i=0;
+  printf("enter the value of n:");
+  scanf("%d" , &n);
+  do
+  {
+      i++;
+      printf("%d\n" , 2*i);
+  }
+  while (i<n);
+  return 0;
+}
+
