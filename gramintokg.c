cf73843617a2b36b3b0d0db092eb25231add00b8
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:42:19 +0530

    Add files via upload

---
+++ gramintokg.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ float gram,kg;
+ printf("enter the gram: ");
+ scanf("%f",&gram);
+
+ kg= gram/1000;
+ printf("The answer of gram in to kg is %f",kg);
+
+ return 0;
+
+}
