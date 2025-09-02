73b9b056ac9093613e928331dee84aa8c0dee4c6
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:39:55 +0530

    Add files via upload

---
+++ divide.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2,divide;
+ printf("the value of num1: ");
+ scanf("%d",&num1);
+
+ printf("the value of num2: ");
+ scanf("%d",&num2);
+
+ divide= num1/num2;
+ printf("The dividing of num1 and num2 is %d",divide);
+
+ return 0;
+
+}
