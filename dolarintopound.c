07adc96fe732e82e0bf2bf22a9f428edc522b6f2
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:42:07 +0530

    Add files via upload

---
+++ dolarintopound.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ float dolar,pound;
+ printf("enter the dolar: ");
+ scanf("%f",&dolar);
+
+ pound=dolar*48/70;
+ printf("the value of pound %f",pound);
+
+
+ return 0;
+
+}
