c10806e4ba38f59aa5e7505a27c3fbea2694fdb6
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:44:22 +0530

    Add files via upload

---
+++ areaperaofrect.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int A,P,L,B;
+ printf("the value of L: ");
+ scanf("%d",&L);
+  printf("the value of B: ");
+   scanf("%d",&B);
+
+ A= L*B;
+ P=2*(L+B);
+ printf("The area of rectangle is %d\n",A);
+printf("The parameter of rectangle is %d\n",P);
+
+ return 0;
+
+}
