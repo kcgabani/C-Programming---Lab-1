b2623b847bc239de9b8974e9c08b4af71b431190
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:42:33 +0530

    Add files via upload

---
+++ kgintogram.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ float kg,gram;
+ printf("enter the kg: ");
+ scanf("%f",&kg);
+
+ gram= kg*1000;
+ printf("The answer of kg in to gram is %f",gram);
+
+ return 0;
+
+}
