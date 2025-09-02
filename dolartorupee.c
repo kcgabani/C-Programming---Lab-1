c04f1dffb22aaca9f7678bcd2d185953b5996239
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:41:53 +0530

    Add files via upload

---
+++ dolartorupee.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int dolar,mul;
+ printf("enter the dolar: ");
+ scanf("%d",&dolar);
+
+ mul=dolar*48;
+ printf("The value of dolar into ruppee %d",mul);
+
+ return 0;
+
+}
