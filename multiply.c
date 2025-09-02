38f864d8530779d88843932ee23198866cc322b2
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:39:36 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ float num1,num2,multiply;
+ printf("the value of num1: ");
+ scanf("%f",&num1);
+
+ printf("the value of num2: ");
+ scanf("%f",&num2);
+
+ multiply= num1*num2;
+ printf("The multiplication of num1 and num2 is %.3f",multiply);
+
+ return 0;
+
+}
