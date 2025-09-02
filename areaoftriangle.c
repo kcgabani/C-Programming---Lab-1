353f49bc833c2885538192aa7c55b0775dadfdac
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:44:54 +0530

    Add files via upload

---
+++ areaoftriangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ float A,H,L;
+ printf("the value of H: ");
+ scanf("%f",&H);
+  printf("the value of L: ");
+   scanf("%f",&L);
+
+ A= H*L/2;
+ printf("The area of triangle is %f\n",A);
+
+ return 0;
+
+}
