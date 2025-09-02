bff9cfcd0b15f76fd9e6aab1dd7b01a0d6939a79
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:44:08 +0530

    Add files via upload

---
+++ areaperaofsquare.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int A,P,L;
+ printf("the value of L: ");
+ scanf("%d",&L);
+
+ A= L*L;
+ P=4*L;
+ printf("The area of square is %d\n",A);
+printf("The parameter of square is %d\n",P);
+
+ return 0;
+
+}
