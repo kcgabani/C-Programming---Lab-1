d719aead4abfba8402ec0672d94253c6b9496e85
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:41:30 +0530

    Add files via upload

---
+++ ruppetodolar.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int ruppee,divide;
+ printf("enter the ruppee: ");
+ scanf("%d",&ruppee);
+
+ divide=ruppee/48;
+ printf("The value of ruppee into dolar is %d",divide);
+
+ return 0;
+
+}
