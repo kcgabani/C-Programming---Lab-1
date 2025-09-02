9eed2399b336b56290dcd1d28cedaaa875271452
kcgabani <kcgabani06@gmail.com>
2025-09-02 14:40:50 +0530

    Add files via upload

---
+++ hourstomin.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int hours,min;
+ printf("the value of hours: ");
+ scanf("%d",&hours);
+
+ min= hours*60;
+ printf("The answer of hours in to minutes is %d",min);
+
+ return 0;
+
+}
